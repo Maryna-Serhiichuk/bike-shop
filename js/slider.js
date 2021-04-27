4a077e7561e6ce3599b64f8ed7756dd4dddbb585
Maryna-Serhiichuk <marina.serhiichuk@gmail.com>
2021-04-27 14:59:43 +0300

    add new page 'homePage'

---
+++ js/slider.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('.fullscreen-slider').slick({
+		arrows: true,
+		dots: true,
+		speed: 1000,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		pauseOnFocus: false,
+		pauseOnHover: false,
+		pauseOnDotsHover: false,
+		draggable: false,
+		swipe: true
+	});
+})
